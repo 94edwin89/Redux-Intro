99bf74fb8fdb2042347ff73e00c27a39bcea5346
Edwin Jude <edwin.judefrancis@gmail.com>
2024-09-30 00:02:17 +0530

    connection redux app with react

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 
@@ -12,7 +13,9 @@ console.log(store.getState());
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>
 );
 
