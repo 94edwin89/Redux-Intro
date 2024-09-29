0f1a8ff618c792470e9e600ad0c51de63fc3f236
Edwin Jude <edwin.judefrancis@gmail.com>
2024-09-29 20:08:57 +0530

    professional redux file structure state slices

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import CreateCustomer from "./CreateCustomer";
-import Customer from "./Customer";
-import AccountOperations from "./AccountOperations";
-import BalanceDisplay from "./BalanceDisplay";
+import CreateCustomer from "./features/cutomers/CreateCustomer";
+import Customer from "./features/cutomers/Customer";
+import AccountOperations from "./features/accounts/AccountOperations";
+import BalanceDisplay from "./features/accounts/BalanceDisplay";
 
 function App() {
   return (
---
+++ src/features/cutomers/customerSlice.js
@@ -0,0 +1,53 @@
+const initialStateCustomer = {
+    fullName: "",
+    nationalID: "",
+    createdAt: "",
+  };
+  
+  
+  
+  export default function CustomerReducer(state = initialStateCustomer, action) {
+    switch (action.type) {
+      case "customer/createCustomer":
+        return {
+          ...state,
+          fullName: action.payload.fullName,
+          nationalID: action.payload.nationalID,
+          createAt: action.payload.createAt,
+        };
+      case "customer/updateName":
+        return {
+          ...state,
+          fullName: action.payload,
+        };
+  
+      default:
+        return state;
+    }
+  }
+  
+  
+  // store.dispatch({ type: "account/deposit", payload: 500 });
+  // store.dispatch({ type: "account/withdraw", payload: 200 });
+  
+  // store.dispatch({
+  //   type: "acount/requestLoan",
+  //   payload: 1000,
+  //   purpose: "Buy a car",
+  // });
+  
+  // store.dispatch({type:'acount/payLoan'})
+  
+  
+  
+  export function CreateCustomer(fullName, nationalID) {
+    return {
+      type: "customer/createCustomer",
+      payload: { fullName, nationalID, createdAt: new Date().toISOString() },
+    };
+  }
+  
+  export function updateName(fullName) {
+    return { type: "account/updateName", payload: fullName };
+  }
+  
